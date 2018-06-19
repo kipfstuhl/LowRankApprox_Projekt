8c18e3f9eaeef241232ae1971e7dc2de02050110
Kipfstuhl Jonas <jonas.kipfstuhl@t-online.de>
2018-06-19 19:24:35 +0200

    add hosvd for type tten and some tools
    Type tten is for representing a Tucker Tensor
    
    hosvd is implemented for relative error and prescribed ranks
    
    some functionality for the tasks are also added, just for convenience

--- tools.jl
+++ tools.jl
@@ -1,5 +1,54 @@
 
-module tensor
+module tools
+
+export unfolding, folding, mode_n_mult, tten, hosvd
+export get_xi, get_rhs_sin, get_rhs_norm
+
+"""
+some definitions
+"""
+function get_xi(n)
+    collect(1:n)/(n+1)
+end
+
+function get_rhs_sin(xi)
+    n = length(xi)
+    rhs = Array{Float64,3}((n,n,n))
+    for i in 1:n
+        for j in 1:n
+            for k in 1:n
+                rhs[i,j,k] = sin(xi[i]+xi[j]+xi[k])
+            end
+        end
+    end
+    return rhs
+end
+
+function get_rhs_norm(xi)
+    n = length(xi)
+    rhs = Array{Float64,3}((n,n,n))
+    for i in 1:n
+        for j in 1:n
+            for k in 1:n
+                rhs[i,j,k] = sqrt(xi[i]*xi[i] + xi[j]*xi[j] + xi[k]*xi[k])
+            end
+        end
+    end
+    return rhs
+end
+
+function approx_sin(n)
+    xi = get_xi(n)
+    rhs = get_rhs_sin(xi)
+    hosvd(rhs, 1e-4)
+end
+
+function approx_norm(n)
+    xi = get_xi(n)
+    rhs = get_rhs_norm(xi)
+    hosvd(rhs, 1e-4)
+end
+
 
 
 """
@@ -9,6 +58,7 @@ function unfolding(a, n)
     dims = size(a)
     indices = setdiff(1:ndims(a),n)
     reshape(permutedims(a,[n;indices]),dims[n],prod(dims[indices]))
+    #reshape(PermutedDimsArray(a,[n;indices]),dims[n],prod(dims[indices]))
 end
 
 """
@@ -19,6 +69,7 @@ function folding(m, n, dims)
     indices = setdiff(1:length(dims), n)
     a = reshape(m, (dims[n],dims[indices]...))
     permutedims(a,invperm([n;indices]))
+    #PermutedDimsArray(a,invperm([n;indices]))
 end
 
 
@@ -33,5 +84,62 @@ function mode_n_mult(a,n, m)
     folding(b,n,new_dims)
 end
 
+
+type tten{T<:Real}
+    core::Array{T}
+    frames::Array{Array{T,2},1}
+
+    
+    
+end
+
+
+"""
+Higher Order Singular Value Decomposition
+"""
+function hosvd(a, ranks::Vector{T}) where {T<:Integer}
+    d = ndims(a)
+    @assert(d==length(ranks), "dimension mismatch")
+
+    frames = Array{Array{Float64,2},1}(d)
+    for i in 1:d
+        U = svd(unfolding(a,i))
+        frames[i] = U[:, 1:ranks[i]]
+    end
+
+    for i in 1:d
+        a = mode_n_mult(a,i,frames[i]')
+    end
+    return tten(a, frames)
+    
+end
+
+function hosvd(a, ranks::Tuple{Vararg{T}}) where {T<:Integer}
+    rank_arr = [ranks...]
+    hosvd(a, rank_arr)
+end
+
+function hosvd(a, tol::T) where {T<:AbstractFloat}
+    d = ndims(a)
+    tol = tol/sqrt(d) * norm(a[:],2)
+
+    frames = Array{Array{Float64,2},1}(d)
+    for i in 1:d
+        U,S = svd(unfolding(a,i))
+        r_i = length(S)
+        for j in 1:length(S)
+            if S[j] < tol
+                r_i = j
+                break
+            end
+        end
+        frames[i] = U[:, 1:r_i]
+    end
+
+    for i in 1:d
+        a = mode_n_mult(a,i,frames[i]')
+    end
+    return tten(a,frames)
+end
 
 end
