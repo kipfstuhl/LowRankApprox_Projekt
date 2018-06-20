05d113de8c3c9614c05436a52852eb6e274dd0d0
Kipfstuhl Jonas <jonas.kipfstuhl@t-online.de>
2018-06-20 22:55:20 +0200

    rewrite hosvd
    In order to improve performance the hosvd function has been rewritten
    Now it uses an algorithm that performs better, because the compression
    is done earlier.
    The old functions are renamed to hosvd2. The new function may only be
    used with an relative tolerance, not with prescribed ranks.
    
    For convenience also a function for forming a full tensor from a
    Tucker Tensor is included.

--- tools.jl
+++ tools.jl
@@ -97,7 +97,41 @@ end
 """
 Higher Order Singular Value Decomposition
 """
-function hosvd(a, ranks::Vector{T}) where {T<:Integer}
+function hosvd(a, tol::T=1e-4) where {T<:AbstractFloat}
+    d = ndims(a)
+    dims = [size(a)...]
+    tol = tol/sqrt(d) * norm(a[:],2)
+    tol2 = tol*tol              # use the sqare for cheaper comparison
+
+    frames = Array{Array{Float64,2},1}(d)
+    for i in 1:d
+        U,S,V = svd(unfolding(a,i))
+        r_i = 1
+        temp = 0.0
+        for j in length(S):-1:1
+            temp += S[j]*S[j]
+            if temp > tol2
+                r_i = j+1       # take the one satisfying the error bound
+                break
+            end
+        end
+        frames[i] = U[:, 1:r_i]
+        #a = mode_n_mult(a,i,frames[i]')
+        # use a more compact representation of the matrix
+        # the implementation of mode_n_mult copys the data around
+        dims[i] = r_i
+        a = folding(diagm(S[1:r_i])*V[:,1:r_i]',i,dims); 
+        gc()                    # garbage collection makes life easier
+    end
+
+    return tten(a,frames)
+end
+
+
+# the hosvd2 functions are worse because they reduce the core at the
+# end when done during the first loop, simultaneous to svd's, it
+# reduces the size and thus cost much
+function hosvd2(a, ranks::Vector{T}) where {T<:Integer}
     d = ndims(a)
     @assert(d==length(ranks), "dimension mismatch")
 
@@ -114,12 +148,12 @@ function hosvd(a, ranks::Vector{T}) where {T<:Integer}
     
 end
 
-function hosvd(a, ranks::Tuple{Vararg{T}}) where {T<:Integer}
+function hosvd2(a, ranks::Tuple{Vararg{T}}) where {T<:Integer}
     rank_arr = [ranks...]
     hosvd(a, rank_arr)
 end
 
-function hosvd(a, tol::T) where {T<:AbstractFloat}
+function hosvd2(a, tol::T) where {T<:AbstractFloat}
     d = ndims(a)
     tol = tol/sqrt(d) * norm(a[:],2)
 
@@ -128,18 +162,34 @@ function hosvd(a, tol::T) where {T<:AbstractFloat}
         U,S = svd(unfolding(a,i))
         r_i = length(S)
         for j in 1:length(S)
+            # rework this
+            # this is not exactly what we want
+            # better is ||S[j:end]|| < tol or even
+            # sum(S[j:end].^2) < tol^2
             if S[j] < tol
                 r_i = j
                 break
             end
         end
         frames[i] = U[:, 1:r_i]
+        gc()
     end
 
     for i in 1:d
         a = mode_n_mult(a,i,frames[i]')
+        gc()
     end
     return tten(a,frames)
+end
+
+
+
+function fullten(a::tten)
+    res = a.core
+    for i in 1:length(a.frames)
+        res = mode_n_mult(res, i, a.frames[i])
+    end
+    return res
 end
 
 end
