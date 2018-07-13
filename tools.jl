b7d7a56d66eb2f48f751371ea46f2cc7ebcc0fa3
Kipfstuhl Jonas <jonas.kipfstuhl@t-online.de>
2018-07-13 12:27:54 +0200

    add function for plotting the singular values

--- tools.jl
+++ tools.jl
@@ -286,3 +286,21 @@ function fullten(a::tten{T}) where {T<:AbstractFloat}
 end
 
 end
+
+
+# plotting
+using Gadfly
+function plot_sing_vals(svs::Array{Array{Float64,1},1})
+    num = length(svs)
+    plts = Array{Any, 1}(num)
+    for i in 1:length(svs)
+        plts[i]=plot(y=svs[i], x=collect(1:length(svs[i])),
+                     Geom.point, Scale.x_discrete, #Scale.y_continuous,
+                     Scale.y_log10,
+                     # Coord.cartesian(fixed=true),
+                     # Geom.subplot_grid(free_y_axis=true),
+                     Guide.title(string("Mode ", i)));
+    end
+    # set_default_plot_size(30cm, 6cm)
+    hstack(plts...)
+end
