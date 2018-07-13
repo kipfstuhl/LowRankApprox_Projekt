b0d0ff717a592a6a9fb626504e6b17c0fe831343
Kipfstuhl Jonas <jonas.kipfstuhl@t-online.de>
2018-07-13 12:27:20 +0200

    use new hosvd function returning the singular values

--- script1.jl
+++ script1.jl
@@ -22,3 +22,12 @@ rhs = get_rhs_norm(xi)
 println("Starting hosvd")
 a2 = hosvd(rhs)
 println("The core tensor has size ", size(a2.core))
+
+
+# for plotting use new function
+rhs = get_rhs_sin(xi)
+a1, s1 = hosvd(rhs, 1e-4, true)
+
+
+rhs = get_rhs_norm(xi)
+a2, s2 = hosvd(rhs, 1e-4, true)
