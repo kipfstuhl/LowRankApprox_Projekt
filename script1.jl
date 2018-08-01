1c49ecef28e880e52ec670b721d312999990bc8a
Jonas Kipfstuhl <jonas.kipfstuhl@t-online.de>
2018-08-01 17:49:25 +0200

    do sensible output and plotting

--- script1.jl
+++ script1.jl
@@ -11,23 +11,33 @@ println("First example with sinus")
 
 rhs = get_rhs_sin(xi)
 println("Starting hosvd")
-a1 = hosvd(rhs)
+a1, s1 = hosvd(rhs, 1e-4, true)
 println("The core tensor has size ",size(a1.core))
-
+residual = rhs - fullten(a1)
+err = norm(residual[:])
+println("The error ||rhs - full(a1)||_F is: ", err)
+println("10⁻⁴ ⋅ ||B₁|| ", 1e-4*norm(rhs[:]))
+println("Relative error is: ", err/norm(rhs[:]))
 
 println()
 println("Second example with norm")
 
 rhs = get_rhs_norm(xi)
 println("Starting hosvd")
-a2 = hosvd(rhs)
-println("The core tensor has size ", size(a2.core))
-
-
-# for plotting use new function
-rhs = get_rhs_sin(xi)
-a1, s1 = hosvd(rhs, 1e-4, true)
-
-
-rhs = get_rhs_norm(xi)
 a2, s2 = hosvd(rhs, 1e-4, true)
+println("The core tensor has size ", size(a2.core))
+residual = rhs - fullten(a2)
+err = norm(residual[:])
+println("The error ||rhs - full(a2)||_F is: ", err)
+println("10⁻⁴ ⋅ ||B₂|| ", 1e-4*norm(rhs[:]))
+println("Relative Error is: ", err/norm(rhs[:]))
+
+println("creating Plots")
+ENV["GKSwstype"]="png"
+using Plots;
+
+p1 = scatter(s1[1],  yaxis=(:log))
+savefig(p1, "report/eigvalb1.png")
+
+p2 = scatter(s2[1], yaxis=(:log))
+savefig(p2, "report/eigvalb2.png")
